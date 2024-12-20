0b618d39c66f6c06699f6b1a48463d645be38083
THUMMA PRASANTH REDDY <52069294+prasanth50@users.noreply.github.com>
2024-12-20 11:38:27 -0500

    Created a Hello world RestController

--- src/main/java/com/prasanth/springboot/myFirstApp/rest/FunRestController.java
+++ src/main/java/com/prasanth/springboot/myFirstApp/rest/FunRestController.java
@@ -1,4 +1,14 @@
 package com.prasanth.springboot.myFirstApp.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FunRestController {
+    // expose "/" to return "Hello, World"
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello world!";
+    }
 }
